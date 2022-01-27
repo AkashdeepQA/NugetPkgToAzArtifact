e92b5a089a53be3c0c83dede6e067a1ab4b2ea43
adoTrainer <akashdeep.ado@outlook.com>
2022-01-27 19:32:01 +0000

    Add project files.

---
+++ NugetPkgToAzArtifact/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NugetPkgToAzArtifact
+{
+    public class Book
+    {
+        public string BookName;
+        public string Isbn;
+        public string Author;
+
+        public Book(string name, string isbn, string author)
+        {
+            BookName = name;
+            Isbn = isbn;
+            Author = author;
+        }
+    }
+}
